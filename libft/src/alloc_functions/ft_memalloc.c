ca9b78f5e82350db9f3e874696f605c1906a44f2
Augustin BOUVEROT <sacrimentocorp@gmail.com>
2018-07-01 12:35:38 +0200

    lib update

--- libft/src/alloc_functions/ft_memalloc.c
+++ libft/src/alloc_functions/ft_memalloc.c
@@ -1,31 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abouvero <abouvero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/08 10:05:44 by abouvero          #+#    #+#             */
-/*   Updated: 2017/12/16 17:08:59 by abouvero         ###   ########.fr       */
+/*   Created: 2017/11/08 13:33:22 by abouvero          #+#    #+#             */
+/*   Updated: 2017/12/16 17:08:55 by abouvero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
-void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+void	*ft_memalloc(size_t size)
 {
+	void	*new_area;
 	size_t	i;
 
 	i = 0;
-	if (!n)
+	if (!(new_area = (char*)malloc(sizeof(char) * size)))
 		return (NULL);
-	while (i < n && ((unsigned char*)src)[i] != (unsigned char)c)
+	while (i < size)
 	{
-		((unsigned char*)dst)[i] = ((unsigned char*)src)[i];
+		((char*)new_area)[i] = 0;
 		i++;
 	}
-	if (i == n)
-		return (NULL);
-	((unsigned char*)dst)[i] = (unsigned char)c;
-	return (&(dst[i + 1]));
+	return (new_area);
 }
